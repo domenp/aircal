5a2a218ea2bd401b74f08e6cfceb3fe5b77f5db3
Domen Pogacnik <domen.pogacnik@gmail.com>
2020-04-19 22:55:57 +0200

    make load dag meta data sql query work in postgres

--- aircal/dao/airflow.py
+++ aircal/dao/airflow.py
@@ -7,7 +7,7 @@ class AirflowDb(object):
         self.engine = engine
 
     def load_dag_metadata(self):
-        return pd.read_sql('SELECT dag_id, schedule_interval FROM dag WHERE is_paused = 0', self.engine)
+        return pd.read_sql('SELECT dag_id, schedule_interval FROM dag WHERE is_paused = \'0\'', self.engine)
 
     def load_dag_run_metadata(self):
         df_dr = pd.read_sql('SELECT dag_id, start_date, end_date FROM dag_run', self.engine)
