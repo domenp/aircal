16d7f8e9a130cc0282eef03acb6f344a1a699522
Domen Pogacnik <domen.pogacnik@gmail.com>
2020-05-12 22:54:59 +0200

    include dao modules to the package

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import pathlib
-from setuptools import setup
+from setuptools import setup, find_packages
 
 BASE_DIR = pathlib.Path(__file__).parent
 README = (BASE_DIR / 'README.md').read_text()
@@ -7,14 +7,14 @@ README = (BASE_DIR / 'README.md').read_text()
 
 setup(
     name='aircal',
-    version='0.1',
+    version='0.1.1',
     description='Export and visualize Airflow DAG runs as events in Google calendar.',
     long_description=README,
     long_description_content_type='text/markdown',
     url='http://github.com/domenp/aircal',
     author='Domen Pogacnik',
     license='MIT',
-    packages=['aircal'],
+    packages=find_packages(),
     install_requires=[
         'numpy',
         'pandas',
